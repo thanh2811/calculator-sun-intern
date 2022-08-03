2e8d7394edf0f3775049401d7db5b83a09357170
ThanhPV <thanhpv@vtvlive.vn>
2022-08-03 16:33:36 +0700

    UI & logic calculator

--- app/src/main/java/com/example/calculatorsunintern/presenter/CalculatorPresenter.kt
+++ app/src/main/java/com/example/calculatorsunintern/presenter/CalculatorPresenter.kt
@@ -5,12 +5,18 @@ import net.objecthunter.exp4j.ExpressionBuilder
 class CalculatorPresenter(private val viewCallback: CalculatorContract.View) :
     CalculatorContract.Presenter {
 
-    override fun expressionIsValidated(expression: String): Boolean{
-        return true
-    }
+    override fun expressionIsValidated(expression: String): Boolean =
+        expression.matches(Regex("^([-+]?\\d+(\\.\\d+)?)?[-+*/]?[-]?(\\d+(\\.\\d+)?)?"))
 
 
     override fun calculate(expression: String) {
-
+        try {
+            val expression2 = ExpressionBuilder(expression).build()
+            viewCallback.showResult(expression2.evaluate())
+        } catch (e: Exception) {
+            println(e.toString())
+            if (e.toString() == "java.lang.ArithmeticException: Division by zero!")
+                viewCallback.onError(e.toString())
+        }
     }
 }
